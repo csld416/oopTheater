868c6ab41e1d8454fee3323944ada310cc2fb4aa
csld <chrischenscience@gmail.com>
2025-04-17 19:13:02 +0800

    some updates

---
+++ src/spec/Nums.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spec;
+
+/**
+ *
+ * @author csld
+ */
+public class Nums {
+    int framesizeX = 950;
+    int frameSizeY = 600;
+    
+    int TopPanelX = 930;
+    int TopPanelY = 120;
+}
