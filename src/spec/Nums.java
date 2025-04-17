99640eace8e6643bf09f60e4f8dfc32bbd48d998
csld <chrischenscience@gmail.com>
2025-04-18 02:11:36 +0800

    add the frame specification numbers

--- src/spec/Nums.java
+++ src/spec/Nums.java
@@ -9,9 +9,10 @@ package spec;
  * @author csld
  */
 public class Nums {
+    //top Panels: [950, 600]
     int framesizeX = 950;
     int frameSizeY = 600;
-    
+    //top Panels: [930, 120]
     int TopPanelX = 930;
     int TopPanelY = 120;
 }
