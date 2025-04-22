828bf0288bc2d2b9854f91030e45cc2a05e0c661
csld <chrischenscience@gmail.com>
2025-04-22 20:30:59 +0800

    add the UI constants

---
+++ src/global/UIConstants.java
@@ -0,0 +1,27 @@
+package global;
+
+public class UIConstants {
+    // === Frame size
+    public static final int FRAME_WIDTH = 946;
+    public static final int FRAME_HEIGHT = 601;
+
+    // === Top Bar
+    public static final int TOP_BAR_HEIGHT = 120;
+
+    // === Movie Card
+    public static final int MOVIE_CARD_WIDTH = 200;
+    public static final int MOVIE_CARD_HEIGHT = 340;
+    public static final int MOVIE_CARD_HGAP = 20;
+    public static final int MOVIE_CARD_VGAP = 30;
+
+    // === TopBar Element Sizes
+    public static final int ICON_PANEL_WIDTH = 120;
+    public static final int ICON_PANEL_HEIGHT = 40;
+
+    // === Logo
+    public static final int LOGO_WIDTH = 120;
+    public static final int LOGO_HEIGHT = 65;
+
+    // etc...
+    public static final int LEFT_PANEL_WIDTH = 250;
+}
