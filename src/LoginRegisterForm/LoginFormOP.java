28b376b2c30332f4c45dd585f13817f050405e52
csld <chrischenscience@gmail.com>
2025-04-25 17:24:47 +0800

    update LR

--- src/LoginRegisterForm/LoginFormOP.java
+++ src/LoginRegisterForm/LoginFormOP.java
@@ -6,7 +6,7 @@ package LoginRegisterForm;
 
 import global.SessionManager;
 import connection.DatabaseConnection;
-import form.StartingPage;
+import MainPage.StartingPage;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Cursor;
