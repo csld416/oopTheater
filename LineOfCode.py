c9b04b914e5b1719452735c23333f45a74f35968
csld <chrischenscience@gmail.com>
2025-04-25 17:25:39 +0800

    Lines of Code

---
+++ LineOfCode.py
@@ -0,0 +1,15 @@
+import os
+
+def count_java_lines(directory):
+    total = 0
+    for root, _, files in os.walk(directory):
+        for file in files:
+            if file.endswith(".java"):
+                file_path = os.path.join(root, file)
+                with open(file_path, 'r', encoding='utf-8') as f:
+                    lines = f.readlines()
+                    total += len(lines)
+    return total
+
+path = "/Users/csld/NetBeansProjects/OOP2025_threater/src"
+print("Total Java lines of code:", count_java_lines(path))
